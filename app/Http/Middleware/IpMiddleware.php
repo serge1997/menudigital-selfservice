e27e481264796bc0c7218731a7b28e29204664ba
serge1997 <serge.gogolo12@gmail.com>
2024-04-07 17:34:09 -0300

    create ip column restaurants table. create and implement middleware logic

---
+++ app/Http/Middleware/IpMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\Restaurant;
+use Illuminate\Support\Facades\Log;
+
+class IpMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::info("Current Ip ". str_replace('.', '', $request->ip()));
+        $appIp = Restaurant::find(Restaurant::RESTAURANT_KEY);
+        if ( str_replace('.', '', $request->ip()) !== $appIp->res_ip ) {
+            return response()->json("unauthorized", 401);
+        }
+        return $next($request);
+    }
+}
