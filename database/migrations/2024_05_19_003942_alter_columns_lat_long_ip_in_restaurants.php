8dadf08f842f636a3a095e01556345e80d2c1d1a
serge1997 <serge.gogolo12@gmail.com>
2024-05-19 00:41:35 -0300

    set longitude latitude ip column to nullable in restaurant table

---
+++ database/migrations/2024_05_19_003942_alter_columns_lat_long_ip_in_restaurants.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('restaurants', function (Blueprint $table) {
+            $table->string("longitude")->nullable();
+            $table->string("latitude")->nullable();
+            $table->string('res_ip')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('restaurants', function (Blueprint $table) {
+            $table->string("longitude");
+            $table->string("latitude");
+            $table->string('res_ip');
+        });
+    }
+};
